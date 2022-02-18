65a41d98a413222f4485a0f519a8f90c12cfa6e5
AAminha <dks4857@gmail.com>
2022-02-19 00:39:47 +0900

    5. 메뉴 구현

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,13 +1,20 @@
 import styles from "./Layout.module.css";
 import Header from "./Header";
 import Menu from "./Menu";
+import { useState } from 'react';
 
-function Layout({children}) {
+function Layout({children, activeMenu}) {
+  const[onoff, setOnOff] = useState(true);
+
+	function onClickMenu() {
+		setOnOff(!onoff);
+	}
+
   return(
     <div className = {styles.container}>
-      <Header />
+      <Header onClickMenu={onClickMenu}/>
 		  <div className={styles.layout}>
-    		<Menu/>
+    		{onoff ? <Menu activeMenu={activeMenu}/> : <div></div>}
 		    <div className = {styles.contents}>{children}</div>
       </div>
     </div>
